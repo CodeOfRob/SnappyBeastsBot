d2de76290ce243c75675ab78058f1dfec30fc87c
Robin <fnacercoding@gmail.com>
2022-08-08 10:34:27 +0200

    fix: filter for bots in no_clients_online exception

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 import logging
-import random
 
 import dotenv
 import telebot
@@ -7,7 +6,7 @@ import ts3
 from telebot.types import BotCommand
 
 from beerlist import Beerlist
-from constants import COMMANDS_RAW, EMOJI_LIST, PRAISE_LIST, TRUSTED_CHAT_IDS
+from constants import COMMANDS_RAW, TRUSTED_CHAT_IDS
 
 
 def get_online_clients(query_uri):
@@ -53,6 +52,8 @@ def main():
     @bot.message_handler(commands=[commands["showonlineclients"].command])
     def on_get_online_clients(msg):
         clients = get_online_clients(query_uri)
+        # filter out clients that dont have client_type == 0 (normal client)
+        clients = [c for c in clients if int(c["client_type"]) == 0]
 
         if len(clients) == 0:
             bot.send_message(msg.chat.id, "no clients online", parse_mode="html")
